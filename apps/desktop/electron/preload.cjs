5554896bfee1748c28f320331d8c4313edc5a24e
Subham Karmakar <rikk4335@gmail.com>
2026-06-22 11:41:30 +0530

    feat: implement electron preload script and configure build scripts for desktop application

--- apps/desktop/electron/preload.cjs
+++ apps/desktop/electron/preload.cjs
@@ -1,6 +1,21 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electronAPI', {
-    getServerPort: () => electron_1.ipcRenderer.invoke('get-server-port'),
+
+// electron/preload.ts
+var import_electron = require("electron");
+import_electron.contextBridge.exposeInMainWorld("electronAPI", {
+  getServerPort: () => import_electron.ipcRenderer.invoke("get-server-port"),
+  getAppVersion: () => import_electron.ipcRenderer.invoke("get-app-version"),
+  restartAndInstall: () => import_electron.ipcRenderer.invoke("restart-and-install"),
+  openExternal: (url) => import_electron.ipcRenderer.invoke("open-external", url),
+  downloadUrl: (url) => import_electron.ipcRenderer.invoke("download-url", url),
+  openCloudLogin: (url) => import_electron.ipcRenderer.invoke("open-cloud-login", url),
+  onUpdateDownloaded: (cb) => {
+    import_electron.ipcRenderer.on("update-downloaded", cb);
+    return () => import_electron.ipcRenderer.removeListener("update-downloaded", cb);
+  },
+  onCloudAuthCallback: (cb) => {
+    const handler = (_event, token) => cb(token);
+    import_electron.ipcRenderer.on("cloud-auth-callback", handler);
+    return () => import_electron.ipcRenderer.removeListener("cloud-auth-callback", handler);
+  }
 });
